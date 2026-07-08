0ca49383572d1830454ed5e22dc3381c7ff62bcd
obrienmak18-maker <obrienmak18@gmail.com>
2026-07-08 23:22:06 +0000

    Final Stability Fix: Return to Kotlin with stable build flags

--- android/app/src/main/java/com/example/clark_vpn/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.clark_vpn;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/clark_vpn/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.example.clark_vpn
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity: FlutterActivity() {}
